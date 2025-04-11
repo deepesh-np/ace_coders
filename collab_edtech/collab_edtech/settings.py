64956c20b138fa22e66e91962fc0a06a0219f5cf
Kasif Seikh <kasifarman000@gmail.com>
2025-04-11 18:24:12 +0530

    3rd

--- collab_edtech/collab_edtech/settings.py
+++ collab_edtech/collab_edtech/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
    
-    'rest_framework_simplejwt',  # ✅ Keep only one!
+    'rest_framework_simplejwt',  
 
     'users',
     'profiles',
